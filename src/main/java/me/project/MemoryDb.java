6dbc00cfa0f92018dd12e4668bee0cc0072ae8d9
PUSHRSP <pushrsp@gmail.com>
2023-12-07 11:42:33 +0900

    이름, 아이디 검색 로직 추가

---
+++ src/main/java/me/project/MemoryDb.java
@@ -0,0 +1,46 @@
+package me.project;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MemoryDb {
+    private final List<Member> arr = new ArrayList<>();
+    private final Map<String, Integer> byUsername = new HashMap<>();
+    private final Map<String, List<Integer>> byName = new HashMap<>();
+
+    public void add(Member member) {
+        if(byUsername.containsKey(member.getUsername())) {
+            return;
+        }
+
+        byUsername.put(member.getUsername(), arr.size());
+
+        if(byName.containsKey(member.getName())) {
+            byName.get(member.getName()).add(arr.size());
+        } else {
+            List<Integer> l = new ArrayList<>();
+            l.add(arr.size());
+            byName.put(member.getName(), l);
+        }
+
+        arr.add(member);
+    }
+
+    public Optional<Member> getByUsername(String username) {
+        if(!byUsername.containsKey(username)) {
+            return Optional.empty();
+        }
+
+        return Optional.of(arr.get(byUsername.get(username)));
+    }
+
+    public List<Member> getByName(String name) {
+        if(!byName.containsKey(name)) {
+            return List.of();
+        }
+
+        return byName.get(name).stream()
+                .map(arr::get)
+                .collect(Collectors.toList());
+    }
+}
